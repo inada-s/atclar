5b0a68b2b1278c6e1df2e0888b84ef112e4df2c3
Shingo Inada <inada-s@klab.com>
2016-08-27 19:32:03 +0900

    initial commit

---
+++ atcoder.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type AtCoderClient struct {
+	URL     string
+	UserID  string
+	Pass    string
+	client  *http.Client
+	isOwner bool
+}
+
+func newAtCoderClient(url, user, password string) *AtCoderClient {
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{Jar: jar}
+	client.Timeout = 5 * time.Second
+	return &AtCoderClient{
+		URL:     url,
+		UserID:  user,
+		Pass:    password,
+		client:  client,
+		isOwner: false,
+	}
+}
+
+func (at *AtCoderClient) GetClars() ([]Clar, error) {
+	resp, err := at.client.Get(at.URL + "/clarifications")
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(resp.Status)
+	}
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var clars []Clar
+	doc.Find("tbody").Find("tr").Each(func(_ int, tr *goquery.Selection) {
+		var clar Clar
+		tr.Find("td").Each(func(i int, td *goquery.Selection) {
+			switch i {
+			case 0:
+				clar.ProblemTitle = td.Text()
+				if path, ok := td.Find("a").Attr("href"); ok {
+					clar.ProblemURL = at.URL + path
+				}
+			case 1:
+				clar.UserID = td.Text()
+				if path, ok := td.Find("a").Attr("href"); ok {
+					clar.UserURL = at.URL + path
+				}
+			case 2:
+				clar.ClarText = td.Text()
+			case 3:
+				clar.ResponseText = td.Text()
+			case 4:
+				clar.IsPublic = td.Text()
+			case 7:
+				if path, ok := td.Find("a").Attr("href"); ok {
+					clar.ReplyURL = at.URL + path
+					slash := strings.LastIndex(path, "/")
+					if slash != -1 && slash+1 != len(path) {
+						clar.ID = path[slash+1:]
+					}
+				}
+			}
+		})
+		clars = append(clars, clar)
+	})
+
+	return clars, nil
+}
+
+func (at *AtCoderClient) Login() error {
+	u, err := url.Parse(at.URL)
+	if err != nil {
+		return err
+	}
+
+	data := url.Values{"name": {at.UserID}, "password": {at.Pass}}
+	resp, err := at.client.PostForm(at.URL+"/login", data)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf(resp.Status)
+	}
+
+	isOwner := false
+	for _, cookie := range at.client.Jar.Cookies(u) {
+		if cookie.Name == "__privilege" && cookie.Value == "owner" {
+			isOwner = true
+		}
+	}
+
+	if !isOwner {
+		return fmt.Errorf("オーナー権限がありません.")
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	conf    Config
+	at      *AtCoderClient
+	clarMap map[string]Clar
+
+	tplSlackQuestion *template.Template
+	tplSlackAnswer   *template.Template
+)
+
+func init() {
+	clarMap = map[string]Clar{}
+	tplSlackQuestion = template.Must(template.New("slackquestion").Parse(`[Clar No.{{.ID}}]
+問題名：{{if .ProblemTitle}} <{{.ProblemURL}}|{{.ProblemTitle}}> {{else}} -{{end}}
+ユーザ名：<{{.UserURL}}|{{.UserID}}>
+質問：{{.ClarText}}
+<{{.ReplyURL}}|質問に回答する>`))
+
+	tplSlackAnswer = template.Must(template.New("slackanswer").Parse(`[Clar No.{{.ID}} に回答しました]
+問題名：<{{.ProblemURL}}|{{.ProblemTitle}}>
+ユーザ名：<{{.UserURL}}|{{.UserID}}>
+質問：{{.ClarText}}
+回答：{{.ResponseText}}
+全体公開：{{.IsPublic}}
+<{{.ReplyURL}}|回答を修正する>`))
+}
+
+type duration struct {
+	time.Duration
+}
+
+func (d *duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
+}
+
+type Config struct {
+	AtCoderURL      string
+	AtCoderUserID   string
+	AtCoderPass     string
+	SlackWebhookURL string
+	CheckInterval   string
+}
+
+type Clar struct {
+	ID           string
+	ProblemTitle string
+	ProblemURL   string
+	UserID       string
+	UserURL      string
+	ClarText     string
+	ResponseText string
+	IsPublic     string
+	ReplyURL     string
+}
+
+func usage() {
+	log.Fatalf("Usage: %s path/to/config", os.Args[0])
+}
+
+func postToSlack(text string) error {
+	params, err := json.Marshal(struct {
+		Text string `json:"text"`
+	}{Text: text})
+
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.PostForm(conf.SlackWebhookURL,
+		url.Values{"payload": {string(params)}})
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf(resp.Status)
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+	}
+	if _, err := toml.DecodeFile(os.Args[1], &conf); err != nil {
+		log.Fatalln("設定ファイルの読み込みに失敗しました.", err)
+	}
+	interval, err := time.ParseDuration(conf.CheckInterval)
+	if interval <= time.Second {
+		interval = time.Second
+	}
+	log.Println(conf.AtCoderURL, "の監視を開始します.")
+	at = newAtCoderClient(conf.AtCoderURL, conf.AtCoderUserID, conf.AtCoderPass)
+	if err := at.Login(); err != nil {
+		log.Fatalln("ログインに失敗しました.", err)
+	}
+	clars, err := at.GetClars()
+	if err != nil {
+		log.Fatalln("初回クラー取得に失敗しました.", err)
+	} else {
+		log.Println("初回クラー取得に成功しました.")
+		log.Println(clars)
+	}
+	for _, clar := range clars {
+		clarMap[clar.ID] = clar
+	}
+	for {
+		time.Sleep(interval)
+		clars, err = at.GetClars()
+		if err != nil {
+			log.Print("クラー取得に失敗しました.", err)
+			log.Print("1分後にリトライします.")
+			time.Sleep(1 * time.Minute)
+			continue
+		}
+		for _, clar := range clars {
+			c, ok := clarMap[clar.ID]
+			if !ok || (clar.ResponseText != c.ResponseText) {
+				var buf bytes.Buffer
+				if ok {
+					tplSlackAnswer.Execute(&buf, clar)
+					log.Println("クラーに回答しました")
+					log.Println(clar)
+				} else {
+					tplSlackQuestion.Execute(&buf, clar)
+					log.Println("新しいクラーがきました", clar)
+					log.Println(clar)
+				}
+				log.Println(buf.Len())
+				if buf.Len() != 0 {
+					err = postToSlack(buf.String())
+					if err != nil {
+						log.Println("Slackへの通知に失敗しました.", err)
+					} else {
+						clarMap[clar.ID] = clar
+					}
+				}
+			}
+		}
+	}
+}
